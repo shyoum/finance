2b7bd9fa4b72429d64ae0e44a9e6633608ea21a2
shyoum <shyoum0328@gmail.com>
2025-08-29 17:02:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import streamlit as st
 import stock_check
-import news_sentiment
+import stock_update
+import news_crawling
+import sentiment_analysis
+import final_selection
 
 def main():
     st.sidebar.title("메뉴")
-    option = st.sidebar.selectbox("기능 선택", ["주가 데이터 획득 및 후보 추출", "뉴스 크롤링 및 감성 분석"])
+    option = st.sidebar.selectbox("선택", [
+        "Check",
+        "Update",
+        "News",
+        "Sentiment",
+        "Select"
+    ])
 
-    if option == "주가 데이터 획득 및 후보 추출":
+    if option == "Check":
         stock_check.run()
-    elif option == "뉴스 크롤링 및 감성 분석":
-        news_sentiment.run()
+    elif option == "Update":
+        stock_update.run()
+    elif option == "News":
+        news_crawling.run()
+    elif option == "Sentiment":
+        sentiment_analysis.run()
+    elif option == "Select":
+        final_selection.run()
 
 if __name__ == "__main__":
     main()
